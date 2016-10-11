e3794ecd1dd51fc77193c97538540151bc9c9f0c
Marc Czolbe <Marc Czolbe@pc001>
2016-10-11 11:03:12 +0200

    Pickinglist, Probleme beim Join

--- src/lists/ArtiList.java
+++ src/lists/ArtiList.java
@@ -52,7 +52,7 @@ public class ArtiList {
 
     public void TabelleHolen() {
         database.DB_Connect con = new database.DB_Connect();
-        result = con.Connect("FROM Arti ORDER BY ANR");
+        result = con.Connect("FROM Arti WHERE LOESCHKENNZEICHEN = ' ' ORDER BY ANR");
     }
 
     public Integer getANR(int row) {
